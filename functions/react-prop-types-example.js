258acadefe75def3d56cb3b3c794d5652a18c446
John <johnsmilga@gmail.com>
2020-09-25 11:25:03 -0700

    react tours api added and additional base key

--- functions/react-prop-types-example.js
+++ functions/react-prop-types-example.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Airtable = require('airtable');
 
 Airtable.configure({ apiKey: process.env.AIRTABLE_API_KEY });
-const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
+const base = Airtable.base(process.env.AIRTABLE_JAVASCRIPT_BASE_ID);
 const table = base.table('store');
 
 exports.handler = async (event, context, callback) => {
--- functions/react-tours-project.js
+++ functions/react-tours-project.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 const Airtable = require('airtable');
 
 Airtable.configure({ apiKey: process.env.AIRTABLE_API_KEY });
-const base = Airtable.base(process.env.AIRTABLE_JAVASCRIPT_BASE_ID);
-const table = base.table('store');
+const base = Airtable.base(process.env.AIRTABLE_REACT_BASE_ID);
+const table = base.table('tours');
 
 exports.handler = async (event, context, callback) => {
   const response = await table.select({}).firstPage();
-  const data = response.map((product) => {
-    delete product.fields.description;
-    return { id: product.id, fields: product.fields };
+  const data = response.map((tour) => {
+    const { name, info, image, price } = tour.fields;
+    return { id: tour.id, name, info, image: image[0].url, price };
   });
   return (
     null,
