205648402d19dd13862061115f6b39f175b7f0f9
John <johnsmilga@gmail.com>
2020-10-01 11:15:56 -0700

    some changes in app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from 'react';
 
 function App() {
-  const getData = async () => {
-    const response = await fetch(
-      'http://localhost:8888/api/javascript-store-single-product?id=rec43w3ipXvP28vo'
-    );
-  };
-  React.useEffect(() => {
-    getData();
-  }, []);
-  return <h1>Hello World</h1>;
+  return <h1>Course API</h1>;
 }
 
 export default App;
